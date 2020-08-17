93662a288c751eb4a2fc189224140adee14bcb9b
rodrigodasilva <ribeiro.rodrigodasilva>
2020-08-17 17:01:15 -0300

    feat: add User authentication

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import profilesRouter from '@modules/users/infra/http/routes/profiles.routes';
 
 const routes = Router();
 
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
+routes.use('/profile', profilesRouter);
 
 export default routes;

53944742b6914299c0227f95351e1a7e9225a53d
rodrigodasilva <ribeiro.rodrigodasilva>
2020-08-17 17:31:03 -0300

    feat: allows the user to Update the profile info

--- src/modules/users/repositories/IUsersRepository.ts
+++ src/modules/users/repositories/IUsersRepository.ts
@@ -3,6 +3,7 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 export default interface IUsersRepository {
   findByEmail(email: string): Promise<User | undefined>;
+  findById(id: string): Promise<User | undefined>;
   create(data: ICreateUserDTO): Promise<User>;
   save(user: User): Promise<User>;
 }
--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -8,6 +8,12 @@ import User from '@modules/users/infra/typeorm/schemas/User';
 class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
 
+  public async findById(id: string): Promise<User | undefined> {
+    const findUser = this.users.find(user => String(user.id) === id);
+
+    return findUser;
+  }
+
   public async findByEmail(email: string): Promise<User | undefined> {
     const findUser = this.users.find(user => user.email === email);
 

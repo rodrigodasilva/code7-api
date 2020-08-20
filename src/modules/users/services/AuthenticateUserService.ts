9b67e5214b2171d2222067cb49703fcd7465b98a
rodrigodasilva <ribeiro.rodrigodasilva>
2020-08-19 23:13:17 -0300

    chore: translate message error in AuthenticateService

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -32,7 +32,7 @@ export default class AuthenticateUserService {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new AppError('Incorrect email/password combination.', 401);
+      throw new AppError('Email e/ou senha incorretos.', 401);
     }
 
     const passwordMatched = await this.hashProvider.comapreHash(
@@ -41,7 +41,7 @@ export default class AuthenticateUserService {
     );
 
     if (!passwordMatched) {
-      throw new AppError('Incorrect email/password combination.', 401);
+      throw new AppError('Email e/ou senha incorretos.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
